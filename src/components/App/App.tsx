2a6830513a087547874ed7a366f1784037e4995c
JustFun <paw93@ukr.net>
2026-03-10 16:00:19 +0200

    ready to start +cafeinfo
    add  react Structure +  add cafeinfo

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import CafeInfo from '../CafeInfo/CafeInfo'
+import css from './App.module.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo/>
+    </div>
+
+    </>
+  )
+}
+
+export default App
